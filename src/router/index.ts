94067db26a7470ccb3c62bbd7b8e5777a5d9b0e0
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-08-23 15:57:41 -0700

    Routes for academies, students, and payments

--- src/router/index.ts
+++ src/router/index.ts
@@ -48,6 +48,60 @@ const router = createRouter({
       component: () => import('@/profile/views/ProfileView.vue'),
       meta: { requiresAuth: true },
     },
+    {
+      path: '/academies',
+      name: 'academy-list',
+      component: () => import('@/academies/views/AcademyListView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/create',
+      name: 'academy-create',
+      component: () => import('@/academies/views/AcademyCreateView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:id/edit',
+      name: 'academy-edit',
+      component: () => import('@/academies/views/AcademyEditView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students',
+      name: 'student-list',
+      component: () => import('@/students/views/StudentListView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students/create',
+      name: 'student-create',
+      component: () => import('@/students/views/StudentCreateView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students/:studentId/edit',
+      name: 'student-edit',
+      component: () => import('@/students/views/StudentEditView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students/:studentId/payments',
+      name: 'payment-list',
+      component: () => import('@/payments/views/PaymentListView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students/:studentId/payments/create',
+      name: 'payment-create',
+      component: () => import('@/payments/views/PaymentCreateView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/academies/:academyId/students/:studentId/payments/:paymentId/edit',
+      name: 'payment-edit',
+      component: () => import('@/payments/views/PaymentEditView.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
 })
 

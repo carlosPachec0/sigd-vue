77babe8be6226015cc30a52b8aaaf4a07d9e61e5
Carlos Cardenas <ing.carlos.pacheco@outlook.com>
2026-08-23 15:59:31 -0700

    Http service methods now return ApiEnvelope<T> instead of T

--- src/shared/api/base-http-service.ts
+++ src/shared/api/base-http-service.ts
@@ -1,26 +1,45 @@
 import type { AxiosRequestConfig } from 'axios'
 import { httpClient } from './httpClient'
 
+export interface ApiEnvelope<T> {
+  message: string
+  data: T
+  status: number
+  errors: string[]
+}
+
 export abstract class BaseHttpService {
   protected readonly client = httpClient
 
-  protected get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return this.client.get<T>(url, config).then((response) => response.data)
+  protected get<T>(url: string, config?: AxiosRequestConfig): Promise<ApiEnvelope<T>> {
+    return this.client.get<ApiEnvelope<T>>(url, config).then((response) => response.data)
   }
 
-  protected post<T, B = unknown>(url: string, body?: B, config?: AxiosRequestConfig): Promise<T> {
-    return this.client.post<T>(url, body, config).then((response) => response.data)
+  protected post<T, B = unknown>(
+    url: string,
+    body?: B,
+    config?: AxiosRequestConfig,
+  ): Promise<ApiEnvelope<T>> {
+    return this.client.post<ApiEnvelope<T>>(url, body, config).then((response) => response.data)
   }
 
-  protected put<T, B = unknown>(url: string, body?: B, config?: AxiosRequestConfig): Promise<T> {
-    return this.client.put<T>(url, body, config).then((response) => response.data)
+  protected put<T, B = unknown>(
+    url: string,
+    body?: B,
+    config?: AxiosRequestConfig,
+  ): Promise<ApiEnvelope<T>> {
+    return this.client.put<ApiEnvelope<T>>(url, body, config).then((response) => response.data)
   }
 
-  protected patch<T, B = unknown>(url: string, body?: B, config?: AxiosRequestConfig): Promise<T> {
-    return this.client.patch<T>(url, body, config).then((response) => response.data)
+  protected patch<T, B = unknown>(
+    url: string,
+    body?: B,
+    config?: AxiosRequestConfig,
+  ): Promise<ApiEnvelope<T>> {
+    return this.client.patch<ApiEnvelope<T>>(url, body, config).then((response) => response.data)
   }
 
-  protected delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    return this.client.delete<T>(url, config).then((response) => response.data)
+  protected delete<T>(url: string, config?: AxiosRequestConfig): Promise<ApiEnvelope<T>> {
+    return this.client.delete<ApiEnvelope<T>>(url, config).then((response) => response.data)
   }
 }
